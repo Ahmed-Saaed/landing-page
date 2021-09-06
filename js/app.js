5d5461ba9fccc99fe3ad3d1cc9df35528f605df0
Ahmed-Saaed <86948480+Ahmed-Saaed@users.noreply.github.com>
2021-09-06 23:47:33 +0200

    add active class event on click

--- js/app.js
+++ js/app.js
@@ -36,37 +36,44 @@ console.log(sections)
  * Begin Main Functions
  * 
 */
-
 // build the nav
 for (let i = 0 ; i < sections.length ;  i++) {
   let navItem = document.createElement('li');
   navLink = document.createElement("a")
-  navLink.innerHTML = sections[i].getAttribute('id');
+  navLink.innerHTML = sections[i].getAttribute('data-nav');
   navLink.setAttribute('href' , '#'+sections[i].getAttribute('id'));
   navItem.appendChild(navLink)
   myNavList.appendChild(navItem)
 }
-let navItems = myNavList.children
+let navItems = myNavList.children;
+let navAnchors = [];
 
-console.log(navItems)
+for (anchor of navItems) {
+  navAnchors.push(anchor.children[0])
+}
+
+console.log(navAnchors)
+
 
 // Add class 'active' to section when near top of viewport
 
-const addActive = function(){
+let addActive = function(){
 for (const section of sections) {
   if (section.classList.contains("active")) {
       section.classList.remove("active");
   }
-};
+}
 for (const section of sections) {
-  if (section.getBoundingClientRect().top < 50) {
+  if (section.getBoundingClientRect().top < 1) {
     section.classList.add("active");
   }
-  }
-}
+  };
+};
 
 addActive()
 // Scroll to anchor ID using scrollTO event
+
+
 
 /**
  * End Main Functions
@@ -79,5 +86,14 @@ addActive()
 // Scroll to section on link click
 
 // Set sections as active
+for (const anch of navAnchors){
+anch.addEventListener("click",function(){
+  console.log(anch.innerHTML)
+  for (const section of sections) {
+    if (section.getAttribute("data-nav") === anch.innerHTML) {
+      section.classList.add("active");
+    }
+    };
+})}
 
 
