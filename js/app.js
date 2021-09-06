0e81312dac91d366b955d11737aeec791adf1646
Ahmed-Saaed <86948480+Ahmed-Saaed@users.noreply.github.com>
2021-09-07 01:04:14 +0200

    fixed the active class on scroll

--- js/app.js
+++ js/app.js
@@ -21,21 +21,8 @@ const myNavList = document.getElementById("navbar__list");
 let main = document.querySelector('MAIN');
 let sections = main.querySelectorAll('MAIN SECTION')
 
-console.log(sections)
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-// gettin the number of my sections 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 // build the nav
 for (let i = 0 ; i < sections.length ;  i++) {
   let navItem = document.createElement('li');
@@ -45,14 +32,14 @@ for (let i = 0 ; i < sections.length ;  i++) {
   navItem.appendChild(navLink)
   myNavList.appendChild(navItem)
 }
+
+// get the anchors in array to use it in the event
 let navItems = myNavList.children;
 let navAnchors = [];
 
 for (anchor of navItems) {
   navAnchors.push(anchor.children[0])
 }
-
-console.log(navAnchors)
 
 
 // Add class 'active' to section when near top of viewport
@@ -64,36 +51,36 @@ for (const section of sections) {
   }
 }
 for (const section of sections) {
-  if (section.getBoundingClientRect().top < 1) {
+  if (section.getBoundingClientRect().top > 1) {
     section.classList.add("active");
   }
   };
 };
 
-addActive()
 // Scroll to anchor ID using scrollTO event
-
-
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
+window.addEventListener('scroll' , addActive)
 // Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+// Set sections as active on click
 for (const anch of navAnchors){
-anch.addEventListener("click",function(){
-  console.log(anch.innerHTML)
+anch.addEventListener('click',function(){
+  for (const section of sections) {
+    if (section.classList.contains("active")) {
+        section.classList.remove("active");
+    }
+  }
   for (const section of sections) {
     if (section.getAttribute("data-nav") === anch.innerHTML) {
       section.classList.add("active");
     }
     };
 })}
+
+
 
 
